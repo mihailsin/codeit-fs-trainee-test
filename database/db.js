1dd16884a1b6d21b5e9416a7675f075c5f509aec
mihailsin <mihailsin88@gmail.com>
2022-05-02 20:07:00 +0300

    remove commentaries in db.js

--- database/db.js
+++ database/db.js
@@ -1,14 +1,6 @@
 const { Pool, Client } = require("pg");
 require("dotenv").config();
 
-// const devConfig = {
-//   user: process.env.USER,
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT,
-// };
-
 const devConfig = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
 
 const productionConfig = {
