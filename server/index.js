78313acf963e47eb62831b4363dc4c7963d10726
mihailsin <mihailsin88@gmail.com>
2022-05-02 16:22:29 +0300

    PREDEPLOY COMMIT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3001;
 const authRouter = require("./routes/authRoute");
 const dashboardRouter = require("./routes/dashboard");
 const countriesRouter = require("./routes/countriesRoute");
@@ -10,6 +11,6 @@ app.use(express.json());
 app.use("/dashboard", dashboardRouter);
 app.use("/auth", authRouter);
 app.use("/countries", countriesRouter);
-app.listen(3001, () => {
-  console.log("server is running on 3001");
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
 });
